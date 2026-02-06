49dac628594003c37d9ab6fa6efe44a306852669
Yam C Borodetsky <yam@yam.codes>
2026-02-07 01:59:21 +0500

    Implement palindrome number check and remove dokka plugin.

--- src/main/java/codes/yam/leetcode/palindromenumber/Solution.java
+++ src/main/java/codes/yam/leetcode/palindromenumber/Solution.java
@@ -16,6 +16,14 @@ class Solution {
      * @return true if x is a palindrome, false otherwise.
      */
     public boolean isPalindrome(int x) {
-        return true;
+        if (x < 0) return false;
+
+        int reversed = 0;
+        int original = x;
+        while (x != 0) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+        return original == reversed;
     }
 }
